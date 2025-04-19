e7f17babe9b46b74433b59a192784030e15b9327
Anant Mishra <79310116+pihusinhaa@users.noreply.github.com>
2025-04-20 01:40:09 +0530

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /* eslint-disable no-undef */
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
     extend: {
       fontFamily: {
